501b2cbc4ba1cb2175be1acf7104ee55a19d2ca4
Miles <59733060+MilesGlitch@users.noreply.github.com>
2024-08-08 21:05:27 +0300

    Update

--- src/main/java/me/milesglitch/statusplugin/Status.java
+++ src/main/java/me/milesglitch/statusplugin/Status.java
@@ -1,30 +1,47 @@
-package me.milesglitch.statusplugin.playerstate;
+package me.milesglitch.statusplugin;
 
+import io.netty.buffer.ByteBuf;
 import io.netty.buffer.Unpooled;
-import me.milesglitch.statusplugin.StatusPlugin;
+import me.milesglitch.statusplugin.playerstate.PlayerState;
 import net.minecraft.network.FriendlyByteBuf;
 import net.minecraft.network.chat.Component;
 import net.minecraft.network.protocol.game.ClientboundSetSubtitleTextPacket;
 import net.minecraft.network.protocol.game.ClientboundSetTitleTextPacket;
 import net.minecraft.server.MinecraftServer;
 import net.minecraft.server.level.ServerPlayer;
+import org.bukkit.Bukkit;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerBedEnterEvent;
 import org.bukkit.event.player.PlayerJoinEvent;
 import org.bukkit.event.player.PlayerQuitEvent;
+import org.bukkit.plugin.java.JavaPlugin;
+import org.bukkit.plugin.messaging.Messenger;
 import org.bukkit.plugin.messaging.PluginMessageListener;
 import org.jetbrains.annotations.NotNull;
 
-import java.util.*;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import java.util.UUID;
 import java.util.concurrent.ConcurrentHashMap;
+import java.util.logging.Logger;
 
-public class PlayerStateManager implements Listener, PluginMessageListener {
-    private final ConcurrentHashMap<UUID, PlayerState> states;
+public class Status extends JavaPlugin implements Listener, PluginMessageListener {
+    private final ConcurrentHashMap<UUID, PlayerState> states = new ConcurrentHashMap<>();;
+    public static Logger log;
+    public static Status PLUGIN;
 
-    public PlayerStateManager(){
-        states = new ConcurrentHashMap<>();
+    @Override
+    public void onEnable() {
+        PLUGIN = this;
+        log = PLUGIN.getLogger();
+        Messenger msg = Bukkit.getMessenger();
+        msg.registerOutgoingPluginChannel(this, "status:state");
+        msg.registerIncomingPluginChannel(this, "status:state", this);
+        msg.registerOutgoingPluginChannel(this, "status:states");
+        super.onEnable();
     }
 
     @EventHandler
@@ -50,21 +67,25 @@ public class PlayerStateManager implements Listener, PluginMessageListener {
     private void notifyPlayer(PlayerJoinEvent event) {
         FriendlyByteBuf buf = new FriendlyByteBuf(Unpooled.buffer());
         playerStatesToBytes(states, buf);
-        event.getPlayer().sendPluginMessage(StatusPlugin.PLUGIN, "status:states", buf.array());
-        broadcastState(new PlayerState(event.getPlayer().getUniqueId()));
+        byte[] dataBytes = new byte[buf.writerIndex()];
+        buf.getBytes(0, dataBytes);
+        event.getPlayer().sendPluginMessage(this, "status:states", dataBytes);
+        broadcastState(this, new PlayerState(event.getPlayer().getUniqueId()));
     }
 
     @EventHandler
     private void removePlayer(PlayerQuitEvent event){
         states.remove(event.getPlayer().getUniqueId());
-        broadcastState(new PlayerState(event.getPlayer().getUniqueId()));
+        broadcastState(this, new PlayerState(event.getPlayer().getUniqueId()));
     }
 
-    private void broadcastState(PlayerState state) {
+    private void broadcastState(JavaPlugin plugin, PlayerState state) {
         FriendlyByteBuf buf = new FriendlyByteBuf(Unpooled.buffer());
         state.toBytes(buf);
-        StatusPlugin.PLUGIN.getServer().getOnlinePlayers().forEach(player ->
-                player.sendPluginMessage(StatusPlugin.PLUGIN, "status:state", buf.array())
+        byte[] dataBytes = new byte[buf.writerIndex()];
+        buf.getBytes(0, dataBytes);
+        plugin.getServer().getOnlinePlayers().forEach(player ->
+                player.sendPluginMessage(Status.PLUGIN, "status:state", dataBytes)
         );
     }
 
@@ -91,11 +112,11 @@ public class PlayerStateManager implements Listener, PluginMessageListener {
     @Override
     public void onPluginMessageReceived(@NotNull String channel, @NotNull Player player, byte @NotNull [] message) {
         if (channel.equals("status:state")) {
-            FriendlyByteBuf buf = new FriendlyByteBuf(Unpooled.wrappedBuffer(message));
+            FriendlyByteBuf buf = new FriendlyByteBuf(Unpooled.copiedBuffer(message));
             PlayerState state = PlayerState.fromBytes(buf);
             state.setPlayer(player.getUniqueId());
             states.put(player.getUniqueId(), state);
-            StatusPlugin.PLUGIN.getLogger().info("[PlayerStatePacket] " + state);
+            broadcastState(this, state);
         }
     }
 }
